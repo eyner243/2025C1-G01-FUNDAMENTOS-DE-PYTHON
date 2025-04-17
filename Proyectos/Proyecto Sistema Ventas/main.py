51482d49f532cf9bb77d55226a74f74acc1a3895
eyner243 <eyner243@hotmail.com>
2025-04-16 21:19:31 -0600

    Clase 13

--- Proyectos/Proyecto Sistema Ventas/main.py
+++ Proyectos/Proyecto Sistema Ventas/main.py
@@ -4,12 +4,21 @@ Autor: Eyner Serrano
 Fecha: 2025-04-16
 Versión: 0.1
 """
+import os
+from modulo import ingresar_ventas, guardar_ventas,analisis_ventas
+
+
+def limpiar_pantalla():
+    """Limpia la pantalla de la terminal en ejecucio"""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def pausar():
     input('\nPresione ENTER para continuar...')
 
+
 #Menu Principal
 def menu():
+    ventas = []
     while True:
         print('\n----- Menú Pricipal -----')
         print('1. Ingresar ventas de cursos UMCA')
@@ -19,13 +28,19 @@ def menu():
         opcion = input('Ingrese una opción: ')
         
         if opcion == "1":
+            limpiar_pantalla()
             print('\n --- Ingreso de Ventas de curso ---')
+            ingresar_ventas(ventas)
             pausar()
         elif opcion == "2":
+            limpiar_pantalla()
             print('\n --- Guardar Ventas en CSV ---')
+            guardar_ventas(ventas)
             pausar()
         elif opcion == "3":
+            limpiar_pantalla()
             print('\n --- Analisis de Ventas ---')
+            analisis_ventas()
             pausar()
         elif opcion == "4":
             print('\n --- Gracias por usar el sistema. Hasta Pronto!')
--- Proyectos/Proyecto Sistema Ventas/modulo.py
+++ Proyectos/Proyecto Sistema Ventas/modulo.py
@@ -1,7 +1,11 @@
+import csv, os, pandas as pd
+
+
+
 def ingresar_ventas(lista_ventas):
     while True:
         try:
-            curso = input ('Íngrese el nombre del curso: ').upper()
+            curso = input('Ingrese el nombre del curso: ').upper()
             cantidad = int (input('Ingrese la cantidad de curso vendidos: '))
             fecha = input('Íngrese la fecha de la venta (AAAA-MM-DD): ')
             precio = float(input('Íngrese el precio del curso: '))
@@ -26,3 +30,37 @@ def ingresar_ventas(lista_ventas):
             break
         else: 
             print ('Opción no valida')
+            
+            
+def guardar_ventas(ventas):
+    if not ventas:
+        print('No hay ventas que guardar en el CSV')
+    else:
+        if os.path.exists('ventas.csv'):
+            #si el archivo existe agrego Append  'A'
+            with open('ventas.csv','a',newline='',encoding='utf-8') as archivo:
+                guardar = csv.DictWriter(archivo,fieldnames=['curso','cantidad','precio','fecha','cliente'])
+                guardar.writerows(ventas)        
+        else: #Si no existe abro en modo escritura 'W'
+            with open('ventas.csv','w',newline='',encoding='utf-8') as archivo:
+                guardar = csv.DictWriter(archivo,fieldnames=['curso','cantidad','precio','fecha','cliente'])
+                guardar.writeheader()
+                guardar.writerows(ventas)
+                
+        #Limpio las ventas en memoria y muestro el guardado exitoso                
+        ventas = []
+        print('Datos guardados exitosamente!')
+        
+        
+def analisis_ventas():
+    df = pd.read_csv('ventas.csv')
+    print('\n --------- RESUMEN DE VENTAS ---------')
+    
+    df['subtotal'] = df['cantidad'] * df['precio']     
+    total_ingresos = df['subtotal'].sum()
+    #Total de ventas
+    print(f'Total de ventas {total_ingresos:.2f}')
+    
+    #Curso mas vendido
+    curso_top = df.groupby('curso')['cantidad'].sum().idxmax()
+    print(f'El curso mas vendido es: {curso_top}')
